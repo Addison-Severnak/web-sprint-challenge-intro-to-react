c7393d9ca463a8c69b1a5e04b30ce0a08d4945ae
Addison Severnak <addisonsevernak@gmail.com>
2022-12-30 13:28:12 -0600

    styling of characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,23 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+display: flex;
+font-size: 40px;
+flex-direction: column;
+padding: 8px;
+color: #388697;
+text-shadow: 1px 1px 5px #fff
+`
 
 const Character = props => {
     return (
-        <div className='character'>
-            {props.info.name}
-        </div>
+        <StyledCharacter className="character">
+            <div>
+                {props.info.name}
+            </div>
+        </StyledCharacter>
     )
 }
 
